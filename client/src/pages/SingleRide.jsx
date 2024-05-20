a437acde16db661a1a06e4102bc820e8ec91a719
Badrul Borhanudin <badrulborhanudin@gmail.com>
2024-05-20 14:57:41 +0800

    forgot to update singleride page layout

--- client/src/pages/SingleRide.jsx
+++ client/src/pages/SingleRide.jsx
@@ -4,6 +4,7 @@ import { Box, Heading, Text, Spinner, Divider } from '@chakra-ui/react';
 
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
+import Layout from '../components/Layout';
 
 import { QUERY_SINGLE_RIDE } from '../utils/queries';
 
@@ -21,41 +22,43 @@ const SingleRide = () => {
   }
 
   return (
-    <Box my={3}>
-      <Heading
-        as='h3'
-        size='lg'
-        bg='blue.500'
-        color='white'
-        p={2}
-        mb={4}
-        borderRadius='md'
-      >
-        {ride.rideAuthor} <br />
-        <Text fontSize='md'>posted this ride on {ride.createdAt}</Text>
-      </Heading>
-      <Box bg='gray.100' py={4} px={2} borderRadius='md'>
-        <Text
-          fontSize='xl'
-          fontStyle='italic'
-          border='2px #1a1a1a'
-          p={4}
+    <Layout>
+      <Box my={3}>
+        <Heading
+          as='h3'
+          size='lg'
+          bg='blue.500'
+          color='white'
+          p={2}
           mb={4}
+          borderRadius='md'
         >
-          Origin: {ride.origin} <br />
-          Destination: {ride.destination} <br />
-          Date: {ride.date} <br />
-          Time: {ride.time}
-        </Text>
+          {ride.rideAuthor} <br />
+          <Text fontSize='md'>posted this ride on {ride.createdAt}</Text>
+        </Heading>
+        <Box bg='gray.100' py={4} px={2} borderRadius='md'>
+          <Text
+            fontSize='xl'
+            fontStyle='italic'
+            border='2px #1a1a1a'
+            p={4}
+            mb={4}
+          >
+            Origin: {ride.origin} <br />
+            Destination: {ride.destination} <br />
+            Date: {ride.date} <br />
+            Time: {ride.time}
+          </Text>
+        </Box>
+
+        <Box my={5}>
+          <CommentList comments={ride.comments} rideId={ride._id} />
+        </Box>
+        <Box m={3} p={4} borderRadius='md'>
+          <CommentForm rideId={ride._id} />
+        </Box>
       </Box>
-      
-      <Box my={5}>
-        <CommentList comments={ride.comments} rideId={ride._id} />
-      </Box>
-      <Box m={3} p={4} borderRadius='md'>
-        <CommentForm rideId={ride._id} />
-      </Box>
-    </Box>
+    </Layout>
   );
 };
 
